bb37db9af1d393e94c6fb2cc9dbb94d56c92ceb3
NicoxRC <nicorojas005@gmail.com>
2022-09-12 14:04:58 -0500

    p

--- 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import s from "../Styles/SearchBar.module.css";
 export default function SearchBar(props) {
   return (
     <div className={s.container}>
-      <input type="text" placeholder="Ciudad..." />
+      <input type="text" className={s.input} placeholder="Ciudad..." />
       <button className={s.btn} onClick={props.onSearch}>
         Agregar
       </button>
